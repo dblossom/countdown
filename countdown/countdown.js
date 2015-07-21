8e636377f5370164da87ff27b6efaf7a63aee37d
Dan Blossom <daniel.blossom@gmail.com>
2015-07-21 14:33:38 -0400

    Fixed the days being off by 1 after noon...
    Since this is a count down to NOON -- ( or any time passed with hours and minutes to an exact hour ) the day would be still say the number of days once the hour past. IE: If noon was the time at 1130 it would have said 75 days, 0 hours, .... then after noon, it would say 75 days, 23 hours - rather than 74 days and 23 hours.
    
    I also dropped seconds for now until I get it worked out - it does not sync with minutes, reason because I want hole numbers but milliseconds is a decimal.

--- countdown/countdown.js
+++ countdown/countdown.js
@@ -19,6 +19,7 @@ function load(){
 * @param hour - the hour (24 hour time)
 */
 function countDown(year, month, day, hour){
+	
 	// get todays date in milliseconds
 	var today = new Date().getTime();
 	
@@ -26,31 +27,18 @@ function countDown(year, month, day, hour){
 	var later = new Date(year, (month - 1), day, hour, 0).getTime();
 
 	// so we need to convert milliseconds to something that will be readable
-	var sec = Number(((later - today) / 1000)).toFixed(0); // 1000 milliseconds in second
-	var min = Number(sec / 60).toFixed(0); // 60 seconds in min
-	var hour = Number(min / 60).toFixed(0); // 60 minutes in hour
-	var days = Number(hour / 24).toFixed(0); // 24 hours in day
+	var sec = Number(((later - today) / 1000)); //.toFixed(0); // 1000 milliseconds in second
+	var min = Number(sec / 60); //.toFixed(0); // 60 seconds in min
+	var hour = Number(min / 60); //.toFixed(0); // 60 minutes in hour
+	var days = Number(hour / 24);//.toFixed(1); // 24 hours in day
 	
 	// mod them so we only see < the day, or < hour, or < min
 	// TODO: consider doing it in one step above, that could remove from readability.
 	sec %= 60;
 	min %= 60;
 	hour %= 24;
-	
-	// hack band-aid ...
-	// so for some reason we are off by 30 seconds
-	// IE: once 29 seconds hits the minutes decrease
-	// so we do this ugly fucking thing until we figure out why...
-	//TODO: at zero it flips to 60 I think ... ? so need a better
-	//      band-aid
-	if (sec < 30){
-		    min +=1;
-	}
-	
-	// FINALLY return the damn result, as a string.
-	return Number(days).toFixed(0) + " days, " +
-	       Number(hour).toFixed(0) + " hours, " +
-	       Number(min).toFixed(0) + " minutes, " +
-	       Number(sec).toFixed(0) + " seconds";
-	
+
+	return Math.floor(days) + " days, " +
+	       Math.floor(hour) + " hours, " +
+	       Math.floor(min) + " minutes!";
 }
