eb7c6a5227da396d5ffbd123b08d8e61c57a13fd
Dan Blossom <daniel.blossom@gmail.com>
2015-08-19 11:33:00 +0000

    Update countdown.js

--- countdown/countdown.js
+++ countdown/countdown.js
@@ -9,7 +9,7 @@ function load(){
 	
         context.clearRect(0,0,canvas.width, canvas.height);
         context.fillText("The time now is: ".concat(new Date().getTime().toDateString()), 0, 25);
-	context.fillText(countDown(2015, 9, 19, 12), 0, 100);	
+	context.fillText(countDown(2015, 9, 19, 12), 0, 50);	
 }
 
 /**
