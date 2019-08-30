e5e91a867c258a7bc7a5387f177c13f07f339407
Dan Blossom <daniel.blossom@gmail.com>
2019-08-30 14:34:50 -0400

    Update countdown.js
    Add some params to load() function

--- countdown/countdown.js
+++ countdown/countdown.js
@@ -2,13 +2,13 @@
 /**
  * Simple function that loads the countdown() for the canvas.
  */
-function load(){ 
+function load(year, month, day, hour){ 
 	var canvas = document.getElementById('canvas');
 	var context = canvas.getContext('2d');
 	context.font = "bold 20px Arial";
 	
     context.clearRect(0,0,canvas.width, canvas.height);
-	context.fillText(countDown(2018, 9, 15, 12), 0, 25);
+	context.fillText(countDown(year, month, day, hour), 0, 25);
 }
 
 /**
